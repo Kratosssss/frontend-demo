0aff762d39a01b92bad9b6b366012d633d8b837a
yt <yaotao@yaotaodeMacBook-Air.local>
2026-08-11 14:25:02 +0800

    chore: add Sites deployment support

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist/client',
+  },
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
